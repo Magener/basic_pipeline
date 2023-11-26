c4098f2cbf1876467373825391f16ff57300f8d8
Dan Shpaner <dan_shpaner@gmail.com>
2023-11-26 11:07:47 +0200

    feat(RatingSensor.py, RatingReceiver.py): Added basic logging using python's logging library.

--- receiver/RatingReceiver.py
+++ receiver/RatingReceiver.py
@@ -1,13 +1,20 @@
 import json
 import asyncio
 import websockets
+import logging
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+SENSOR_URL = "ws://localhost:8765"
 
 async def listen_for_new_ratings():
-    async with websockets.connect("ws://localhost:8765") as websocket:
+    async with websockets.connect(SENSOR_URL) as websocket:
+        logger.info(f'Connected to {SENSOR_URL}')
         while True:
             rating_data = json.loads(await websocket.recv())
 
-            print(f"Rating data has been received: {rating_data}")
+            logger.info(f"Rating data has been received: {rating_data}")
 
 asyncio.run(listen_for_new_ratings())
--- sensor/RatingSensor.py
+++ sensor/RatingSensor.py
@@ -2,6 +2,11 @@ import random
 import asyncio
 import websockets
 import json
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 from sensor.CSVLoader import generator_from_csv
 
@@ -10,12 +15,15 @@ URL = "localhost"
 RATINGS_FILE_PATH = './Ratings.csv'
 
 async def broadcast_rating_data(websocket, path):
+    logger.info("User has been connected!")
+
     for row in generator_from_csv(RATINGS_FILE_PATH):
         MIN_NEW_RATING_RATE = 0.5
         MAX_NEW_RATING_RATE = 2
         rating_rate = random.uniform(MIN_NEW_RATING_RATE, MAX_NEW_RATING_RATE)
 
         await websocket.send(json.dumps(row))
+        logger.info(f"Rating has been sent! {row}")
         await asyncio.sleep(rating_rate)
 
 async def run_server():
