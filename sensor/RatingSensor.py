3810e2fd8279de5c166092e0372bb96666b17417
Dan Shpaner <dan.shpner9@gmail.com>
2023-11-27 11:45:12 +0200

    feat(RatingSensor.py): Readded delay between ratings.

--- sensor/RatingSensor.py
+++ sensor/RatingSensor.py
@@ -1,8 +1,12 @@
 import json
+import random
+import time
+
 from confluent_kafka.cimpl import Producer
 
 from sensor.CSVLoader import generator_from_csv
-from sensor.consts import RATINGS_FILE_PATH, KAFKA_BROKER_URL, RATING_TOPIC_NAME
+from sensor.consts import RATINGS_FILE_PATH, KAFKA_BROKER_URL, RATING_TOPIC_NAME, MIN_NEW_RATING_RATE, \
+    MAX_NEW_RATING_RATE
 from sensor.log import logger
 
 
@@ -15,11 +19,11 @@ producer = Producer(producer_conf)
 logger.info("User has been connected!")
 
 for row in generator_from_csv(RATINGS_FILE_PATH):
-    # TODO: readd sleep for time.
-    #rating_rate = random.uniform(MIN_NEW_RATING_RATE, MAX_NEW_RATING_RATE)
+    delay_between_ratings = random.uniform(MIN_NEW_RATING_RATE, MAX_NEW_RATING_RATE)
 
     producer.produce(topic=RATING_TOPIC_NAME, value=json.dumps(row))
     logger.info(f"Rating has been sent! {row}")
 
-#TODO: should I move flush to inside of the loop?
+    time.sleep(delay_between_ratings)
+
 producer.flush()
