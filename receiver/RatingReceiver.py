955e1264a6d8452cb718bdeb91bf433fc3d407b8
Dan Shpaner <dan.shpner9@gmail.com>
2023-11-27 12:02:51 +0200

    feat(consts.py, RatingReceiver.py): Basic refactoring

--- receiver/RatingReceiver.py
+++ receiver/RatingReceiver.py
@@ -1,41 +1,28 @@
 import json
-from confluent_kafka.cimpl import Consumer
+from confluent_kafka.cimpl import Consumer, KafkaException
 
-from receiver.consts import KAFKA_BROKER_URL
+from receiver.consts import KAFKA_BROKER_URL, CONSUMER_CONF, RATING_TOPIC_NAME
 from receiver.log import logger
 
-consumer_conf = {
-        'bootstrap.servers': KAFKA_BROKER_URL,
-        'group.id': 'websocket-consumer-group'
-    }
 
-consumer = Consumer(consumer_conf)
-
-# Subscribe to Kafka topic
-topic = 'book_ratings'  # Replace with your Kafka topic
-consumer.subscribe([topic])
+def ensure_message_valid(msg) -> None:
+    if msg.error() and msg.error().code() != KafkaException._PARTITION_EOF:
+        raise SystemExit(msg.error())
 
 
-#TODO: refactor
+consumer = Consumer(CONSUMER_CONF)
+consumer.subscribe([RATING_TOPIC_NAME])
+
 try:
     logger.info(f'Connected to {KAFKA_BROKER_URL}')
     while True:
         msg = consumer.poll(timeout=1.0)
 
-        if msg is None:
-            continue
-        if msg.error():
-            if msg.error().code() == -1:
-                print("some error")
-                continue
-            else:
-                print(msg.error())
-                break
-
-        rating_data = json.loads(msg.value().decode('utf-8'))
-
-        logger.info(f"Rating data has been received: {rating_data}")
-
+        if msg is not None:
+            ensure_message_valid(msg)
+            rating_data = json.loads(msg.value().decode("utf-8"))
+            logger.info(f"Rating data has been received: {rating_data}")
+except SystemExit as e:
+    logger.error(str(e))
 finally:
-    # Close Kafka consumer on exit
     consumer.close()
--- receiver/consts.py
+++ receiver/consts.py
@@ -5,3 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 KAFKA_BROKER_URL = os.getenv('KAFKA_BROKER_URL')
 RATING_TOPIC_NAME = os.getenv('RATING_TOPIC_NAME')
+
+
+
+CONSUMER_CONF = {
+        'bootstrap.servers': KAFKA_BROKER_URL,
+        'group.id': 'websocket-consumer-group'
+    }
