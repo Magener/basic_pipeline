e04e95d2ad13f8212bef35fd9b0049e5cc461e72
Dan Shpaner <dan.shpner9@gmail.com>
2023-11-28 17:07:20 +0200

    feat(MessageValidation.py, RatingReceiver.py): Basic refactoring and error handling if JSON hasn't arrived correctly.

--- receiver/RatingReceiver.py
+++ receiver/RatingReceiver.py
@@ -1,16 +1,12 @@
 import json
+
 from confluent_kafka.cimpl import Consumer
 
+from receiver.MessageValidation import validate_message, extract_rating_data
 from receiver.PostgresConnection import PostgresConnection
 from receiver.consts import KAFKA_BROKER_URL, CONSUMER_CONF, RATING_TOPIC_NAME, CONSUMER_POLL_TIMEOUT
 from receiver.log import logger
 from receiver.postgresql.Review import commit_review
-
-
-def validate_message(msg) -> None:
-    if msg.error():
-        raise RuntimeError(msg.error())
-
 
 consumer = Consumer(CONSUMER_CONF)
 consumer.subscribe([RATING_TOPIC_NAME])
@@ -23,12 +19,8 @@ try:
         if msg:
             validate_message(msg)
             rating_data = json.loads(msg.value())
-            #TODO: ensure type of json fields beforehand
-            commit_review(int(rating_data["User-ID"]), rating_data["ISBN"], int(rating_data["Book-Rating"]))
+            commit_review(*extract_rating_data(rating_data))
             logger.info(f"Saved in DB: {rating_data}")
-except RuntimeError as e:
-    logger.error(str(e))
 finally:
     consumer.close()
-
-PostgresConnection().close()
+    PostgresConnection().close()
