6a336bfaa18dcd8f319bc86e999f154229cce641
Dan Shpaner <dan.shpner9@gmail.com>
2023-11-29 11:16:15 +0200

    feat(InsightAPI.py, log.py): Some refactoring

--- insight_api/InsightAPI.py
+++ insight_api/InsightAPI.py
@@ -2,8 +2,10 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
+from fastapi import status
 
 from insight_api.consts import MIN_RATINGS_FOR_INCLUSION, PORT, HOST
+from insight_api.log import logger
 from insight_api.postgres.AsyncPostgresConnection import AsyncPostgresConnection
 from insight_api.postgres.Review import find_top_rated_books
 
@@ -22,8 +24,8 @@ async def get_books(presented_amount: int = None):
     try:
         return await find_top_rated_books(MIN_RATINGS_FOR_INCLUSION, presented_amount)
     except BaseException as e:
-        print(f"An error has occurred {e}") #TODO; set as a logger.
-        raise HTTPException(status_code=500, detail="An error has occurred")
+        logger.error(f"An error has occurred {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="An error has occurred")
 
 
 if __name__ == "__main__":
