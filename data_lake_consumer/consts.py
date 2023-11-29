4cde2be5f0afd5dacb9dcefa4c4439ffea628c4e
Dan Shpaner <dan.shpner9@gmail.com>
2023-11-29 14:45:38 +0200

    feat(consts.py, DataLakeConsumer.py, log.py): Added a basic consumer for saving originally produced data.

---
+++ data_lake_consumer/consts.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+PATH_TO_DB = os.getenv('PATH_TO_DB')
+KAFKA_BROKER_URL = os.getenv('KAFKA_BROKER_URL')
+RATING_TOPIC_NAME = os.getenv('RATING_TOPIC_NAME')
+CONSUMER_POLL_TIMEOUT = float(os.getenv('CONSUMER_POLL_TIMEOUT'))
+
+
+CONSUMER_CONF = {
+        'bootstrap.servers': KAFKA_BROKER_URL,
+        'group.id': 'websocket-consumer-group'
+    }
