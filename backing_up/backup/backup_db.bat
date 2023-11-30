7af36072a6f937df76a42c0f6cb3668964876c29
Dan Shpaner <dan.shpner9@gmail.com>
2023-11-30 11:59:44 +0200

    feat(.gitignore, backup_db.bat, example_backup_environment.bat, example_connection_data.bat, schedule_backup.bat): Moved files, added ability to take path of connection_data as an argument.

--- backing_up/backup/backup_db.bat
+++ backing_up/backup/backup_db.bat
@@ -1,6 +1,8 @@
-@echo off
+CALL "%1\connection_data.bat"
 
 set DATE=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
 set BACKUP_FILE=%BACKUP_DIR%\%DB_NAME%_backup_%DATE%.sql
 
 %POSTGRES_HOME%\bin\pg_dump.exe -U %DB_USER% -w %DB_NAME% > %BACKUP_FILE%
+
+PAUSE
