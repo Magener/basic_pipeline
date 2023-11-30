10767b1d97998bd7a06130c7fdafa42a11b1aa4b
Dan Shpaner <dan.shpner9@gmail.com>
2023-11-30 14:58:13 +0200

    feat(schedule_backup.bat): Fixed bug if \ is at the end of the BACKUP_SCRIPT_LOCATION string.

--- backing_up/task_management/schedule_backup.bat
+++ backing_up/task_management/schedule_backup.bat
@@ -2,9 +2,9 @@
 
 CALL "./backup_environment.bat"
 
-set ARG_PARSED_COMMAND="%BACKUP_SCRIPT_LOCATION%\backup\backup_db.bat %BACKUP_SCRIPT_LOCATION%\backup\"
+set ARG_PARSED_COMMAND=%BACKUP_SCRIPT_LOCATION%\backup\backup_db.bat %BACKUP_SCRIPT_LOCATION%\backup\
 
-schtasks /create /tn "AutomaticallyBackUp" /tr %ARG_PARSED_COMMAND% /sc DAILY /st 11:33 /ru SYSTEM
+schtasks /create /tn "AutomaticallyBackUp" /tr "%ARG_PARSED_COMMAND% " /sc DAILY /st 14:48 /ru SYSTEM
 
 set ARG_PARSED_COMMAND=
 
