854509831609905aca7401fd8f1097d984fa460b
Dan Shpaner <dan.shpner9@gmail.com>
2023-12-03 15:46:33 +0200

    feat(RatingReceiver.py,Review.py): fixed RatingReceiver.py to work.

--- receiver/rating/RatingReceiver.py
+++ receiver/rating/RatingReceiver.py
@@ -1,12 +1,12 @@
 from receiver.JSONReceiver import JSONReceiver
 from receiver.MessageHandlingStrategy import MessageHandlingStrategy
-from receiver.MessageValidation import extract_rating_data
-from receiver.postgresql.Review import commit_review
+from receiver.MessageValidation import extract_rating_data, catch_data_parsing_errors
+from receiver.rating.Review import commit_review
 
 
 class CommitReview(MessageHandlingStrategy):
     async def on_message(self, data: dict) -> None:
-        await commit_review(*extract_rating_data(data))
+        await commit_review(*catch_data_parsing_errors(data, extract_rating_data))
 
 
 if __name__ == "__main__":
