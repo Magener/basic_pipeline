9cda60136d702b2ac3b50aef3a8624fb023f069a
Dan Shpaner <dan.shpner9@gmail.com>
2023-12-04 11:10:20 +0200

    feat(book_ddl.sql, Review.py, review_ddl.sql): some refactoring & moving files.

--- insight_api/postgres/Review.py
+++ insight_api/postgres/Review.py
@@ -7,14 +7,14 @@ async def find_top_rated_books(minimum_ratings, presented_amount=None):
     optional_query_limitation = "LIMIT $2" if presented_amount else ""
     optional_query_argument = [presented_amount] if presented_amount else []
 
-    QUERY = 'SELECT book_id, AVG(score) as avg_score FROM hafifa.ratings ' \
-            'GROUP BY book_id HAVING COUNT(*) > $1 ' \
-            f'ORDER BY avg_score DESC {optional_query_limitation}'
+    BOOK_SCORE_QUERY = 'SELECT book_id, AVG(score) as avg_score FROM hafifa.ratings ' \
+                       'GROUP BY book_id HAVING COUNT(*) > $1 ' \
+                       f'ORDER BY avg_score DESC {optional_query_limitation}'
 
-    JOINED_BOOK_NAME_QUERY = f"SELECT score.book_id, book.book_name, score.avg_score FROM ({QUERY}) score " \
-                             "LEFT JOIN hafifa.book book ON score.book_id=book.book_id"
+    BOOK_NAME_AND_SCORES_QUERY = f"SELECT score.book_id, book.book_name, score.avg_score FROM ({BOOK_SCORE_QUERY}) score " \
+                                 "LEFT JOIN hafifa.book book ON score.book_id=book.book_id"
 
-    result = await connection.fetch(JOINED_BOOK_NAME_QUERY, minimum_ratings, *optional_query_argument)
+    result = await connection.fetch(BOOK_NAME_AND_SCORES_QUERY, minimum_ratings, *optional_query_argument)
 
     result = [dict(row) for row in result]
 
