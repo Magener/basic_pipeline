189593d70d9912d91f7c8d395f7e50d9fab57d0f
Dan Shpaner <dan.shpner9@gmail.com>
2023-12-04 11:56:58 +0200

    feat(Review.py, consts.py): Removed redundent injection to query

--- receiver/postgresql/Review.py
+++ receiver/postgresql/Review.py
@@ -1,11 +1,10 @@
 from insight_api.postgres.AsyncPostgresConnection import AsyncPostgresConnection
-from receiver.consts import RATING_TABLE_NAME
 from receiver.log import logger
 
 
 async def commit_review(reviewer_id, book_id, score):
     connection = await AsyncPostgresConnection().get_connection()
-    QUERY = f"""INSERT INTO {RATING_TABLE_NAME}(reviewer_id, book_id, score) VALUES ($1, $2, $3);"""
+    QUERY = "INSERT INTO hafifa.ratings(reviewer_id, book_id, score) VALUES ($1, $2, $3);"
 
     result_message = await connection.execute(QUERY, reviewer_id, book_id, score)
 
@@ -15,3 +14,5 @@ async def commit_review(reviewer_id, book_id, score):
         logger.debug(f"Regstered {(reviewer_id, book_id, score)} in database.")
     else:
         raise RuntimeError(f"Insertion has failed: {result_message}")
+
+
