5cfb76d218dac64b916666af88f6c7a73d8ea9a4
Dan Shpaner <dan.shpner9@gmail.com>
2023-12-04 13:57:59 +0200

    feat(consts.py): added default values for .env variables if they were not provided.

--- data_lake_consumer/consts.py
+++ data_lake_consumer/consts.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 PATH_TO_DB = os.getenv('PATH_TO_DB')
 KAFKA_BROKER_URL = os.getenv('KAFKA_BROKER_URL')
-RATING_TOPIC_NAME = os.getenv('RATING_TOPIC_NAME')
-CONSUMER_POLL_TIMEOUT = float(os.getenv('CONSUMER_POLL_TIMEOUT'))
+RATING_TOPIC_NAME = os.getenv('RATING_TOPIC_NAME', "book_ratings")
+CONSUMER_POLL_TIMEOUT = float(os.getenv('CONSUMER_POLL_TIMEOUT', 0.4))
 
 
 CONSUMER_CONF = {
